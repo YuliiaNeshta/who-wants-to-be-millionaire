8a6eac227ef735f85b7aaddcd8661bd720391506
Yuliia <julija.neshta@ro.ru>
2023-06-18 13:14:05 +0300

    add basepath for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Game from './pages/Game';
 import HomePage from './pages/HomePage';
 import Results from './pages/Results';
 
 const App = () => {
   return (
-    <Router>
+    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/who-wants-to-be-millionaire/'}>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/question/:id" element={<Game />} />
         <Route path="/results" element={<Results />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
 
